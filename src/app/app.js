1f5313f23ab0055aa506f14dd1e12ad5fc4a59e3
Peter Todorov <petodorov@gmail.com>
2020-01-13 21:25:02 +0200

    exports and imports .js files

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,8 @@
-const alertService = new AlertService()
-const componentService = new ComponentService()
-const run = (alertService, componentService) => {
+import { inputsAreValid } from './utils/inputs-are-valid'
+import { parseInputs } from './utils/parse-inputs'
+
+
+export const run = (alertService, componentService) => {
     alertService.hideErrors()
     componentService.onClick(() => {
         alertService.hideErrors()
@@ -15,4 +17,3 @@ const run = (alertService, componentService) => {
         }
     })
 }
-run(alertService,componentService)
